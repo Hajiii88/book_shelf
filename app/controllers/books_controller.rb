6abfd28d04a2926d24863872aec8a4984d2678c3
Hajiii88 <kessaku1022@icloud.com>
2019-05-03 17:37:40 +0900

    add new and create of books :)

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,5 +2,18 @@ class BooksController < ApplicationController
   def index 
     @books = Book.all
   end
+  def new
+    @book = Book.new
+  end
+  def create
+    @book = Book.new
+    @book.save
+    redirect_to @book , notice:"書籍を登録しました"
+  end
+end
+private
+def book_params
+  params.require(book).permit(:title, :price ,:publih_date, :description)
+end
 end
 
