b8a422a24ceb06bb8f7bc54d19782aafa3d61b2d
Hajiii88 <kessaku1022@icloud.com>
2019-05-04 16:30:14 +0900

    adddd ruting

--- db/migrate/20190503072400_create_books.rb
+++ db/migrate/20190503072400_create_books.rb
@@ -2,10 +2,11 @@ class CreateBooks < ActiveRecord::Migration[5.2]
   def change
     create_table :books do |t|
       t.string :title,null: false, length: 50
-      t.integer :price, null:false
+      t.integer :price, null:false 
       t.date :publish_date , null:false
-      t.text :description,null:false , length: 1000
+      t.text :description,null:false , length: 500
       t.timestamps
     end
+    add_index :reviews, [:book_id, :user_id], unique: true
   end
 end
--- db/migrate/20190504000453_create_active_storage_tables.active_storage.rb
+++ db/migrate/20190504000453_create_active_storage_tables.active_storage.rb
@@ -2,8 +2,8 @@
 class CreateActiveStorageTables < ActiveRecord::Migration[5.2]
   def change
     create_table :active_storage_blobs do |t|
-      t.string   :key,        null: false
-      t.string   :filename,   null: false
+      t.string   :key,        null: false 
+      t.string   :filename,   null: false 
       t.string   :content_type
       t.text     :metadata
       t.bigint   :byte_size,  null: false
